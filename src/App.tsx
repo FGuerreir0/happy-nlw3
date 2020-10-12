9733696f0440d0d1d84e882fb0788746b6741618
Fabio Guerreiro <ffsguerreiro@hotmail.com>
2020-10-13 00:36:21 +0100

    Landing page created and stylized

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,33 @@
 import React from 'react';
 
+import { FiArrowRight } from 'react-icons/fi';
+
+import './styles/global.css';
+import './styles/pages/landing.css';
+
+import LogoImg from './images/logo.svg';
+import LandingImg from './images/ilustra02.svg';
+
 function App() {
   return (
-    <div className='App'>
-      <h1>
-        Something new!{' '}
-        <span role='img' aria-label='rocket'>
-          🚀
-        </span>
-      </h1>
+    <div id='page-landing'>
+      <div className='content-wrapper'>
+        <img src={LogoImg} alt='Logo' />
+
+        <main>
+          <h1>Leve felicidade para o mundo</h1>
+          <p>Visite orfanatos e mude o dia das crianças.</p>
+        </main>
+
+        <div className='location'>
+          <strong>Rio do Sul</strong>
+          <span>Santa Catarina</span>
+        </div>
+
+        <a href='' className='enter-app'>
+          <FiArrowRight size={26} color='rgba(0,0,0 0.6)' />
+        </a>
+      </div>
     </div>
   );
 }
