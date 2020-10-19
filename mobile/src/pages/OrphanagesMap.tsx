aa3616636bb8b1c077dc789d83ffb6d5baa68c83
Fabio Guerreiro <ffsguerreiro@hotmail.com>
2020-10-19 18:41:40 +0100

    Resolving GitHUb SubFolder problem

---
+++ mobile/src/pages/OrphanagesMap.tsx
@@ -0,0 +1,124 @@
+import React from 'react';
+
+import { StyleSheet, Text, View, Dimensions, TouchableOpacity } from 'react-native';
+import MapView, { Marker, PROVIDER_GOOGLE, Callout } from 'react-native-maps';
+import { Feather } from '@expo/vector-icons';
+import mapMarker from '../images/map-marker.png';
+import { useNavigation } from '@react-navigation/native';
+import { RectButton } from 'react-native-gesture-handler';
+
+export default function OrphanagesMap() {
+  const navigation = useNavigation();
+
+  function handleNavigateToOrphanageDetail() {
+    navigation.navigate('OrphanageDetails');
+  }
+
+  function handleNavigateToCreateOrphanage() {
+    navigation.navigate('SelectMapPosition');
+  }
+
+  return (
+    <View style={styles.container}>
+      <MapView
+        provider={PROVIDER_GOOGLE}
+        style={styles.map}
+        initialRegion={{
+          latitude: 38.5645883,
+          longitude: -9.0460533,
+          latitudeDelta: 0.004,
+          longitudeDelta: 0.004,
+        }}
+      >
+        <Marker
+          icon={mapMarker}
+          calloutAnchor={{
+            x: 2.7,
+            y: 0.8,
+          }}
+          coordinate={{
+            latitude: 38.5645883,
+            longitude: -9.0460533,
+          }}
+        >
+          <Callout tooltip onPress={handleNavigateToOrphanageDetail}>
+            <View style={styles.calloutContainer}>
+              <Text style={styles.calloutText}>Casa do Fábio</Text>
+            </View>
+          </Callout>
+        </Marker>
+      </MapView>
+
+      <View style={styles.footer}>
+        <Text style={styles.footerText}>2 Orfanatos encontrados</Text>
+
+        {/*<TouchableOpacity
+          style={styles.createOrphanageButton}
+          onPress={handleNavigateToCreateOrphanage}
+        >
+          <Feather name='plus' size={20} color='#FFF'></Feather>
+        </TouchableOpacity>*/}
+
+        <RectButton style={styles.createOrphanageButton} onPress={handleNavigateToCreateOrphanage}>
+          <Feather name='plus' size={20} color='#FFF'></Feather>
+        </RectButton>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+
+  calloutContainer: {
+    width: 160,
+    height: 46,
+    paddingHorizontal: 16,
+    backgroundColor: 'rgba(255,255,255, 0.8)',
+    borderRadius: 16,
+    justifyContent: 'center',
+    elevation: 3,
+  },
+
+  calloutText: {
+    color: '#0089a5',
+    fontSize: 14,
+    fontFamily: 'Nunito_700Bold',
+  },
+
+  footer: {
+    position: 'absolute',
+    left: 24,
+    right: 24,
+    bottom: 32,
+
+    backgroundColor: '#FFF',
+    borderRadius: 20,
+    height: 56,
+    paddingLeft: 24,
+
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    elevation: 3, //SOMBRA
+  },
+  footerText: {
+    color: '#8fa7b3',
+    fontFamily: 'Nunito_700Bold',
+  },
+  createOrphanageButton: {
+    width: 56,
+    height: 56,
+    backgroundColor: '#15c3d6',
+    borderRadius: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
